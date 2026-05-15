ddc431683a59ec78b8295b6c2359add8d18e96d1
stefann18 <stefansnikolov18@abv.bg>
2026-05-15 15:52:11 +0300

    Add User Table with getAllUsers

---
+++ src/main/java/bg/sofia/uni/event_management/dto/UserRequest.java
@@ -0,0 +1,4 @@
+package bg.sofia.uni.event_management.dto;
+
+public class UserRequest {
+}
---
+++ src/main/java/bg/sofia/uni/event_management/model/User.java
@@ -0,0 +1,78 @@
+package bg.sofia.uni.event_management.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "email", unique = true, length = 255)
+    private String email;
+
+    @Column(name = "hash_password", nullable = false, length = 255)
+    private String hashPassword;
+
+    @Column(name = "first_name", nullable = false, length = 255)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 255)
+    private String lastName;
+
+    @Column(name = "role", nullable = false, length = 50)
+    private String role;
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getHashPassword() {
+        return hashPassword;
+    }
+
+    public void setHashPassword(String hashPassword) {
+        this.hashPassword = hashPassword;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
